81432170e417c45b00f7b58b1e4c18c129be9bbb
wiserdeveloper <wiserdeveloper@gmail.com>
2025-04-22 16:53:33 -0400

    Getting Started

---
+++ plantpal/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Routes } from "react-router-dom"
+
+import Header from './Header';
+import Homepage from './pages/Homepage';
+
+import './App.css';
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
---
+++ plantpal/src/Header.js
@@ -0,0 +1,10 @@
+import React, { useState, useContext } from "react";
+import { Link } from "react-router"
+
+const Header = () => {
+    return (
+        <h1>Header</h1>
+    )
+}
+
+export default Header
